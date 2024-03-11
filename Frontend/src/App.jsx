ccc3d4e5cc8e08f2b5ca6fc80ba6985fffc606cf
SujitArgade009 <sujitargade009@gmail.com>
2024-03-11 21:40:24 +0530

    New chnages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Footer from './components/Footer.jsx';
-import Header from './components/Header.jsx';
-
+import Footer from './components/Footer/Footer.jsx';
+import Header from './components/Header/Header.jsx';
+import Content from "./components/Contain/Content.jsx";
 
 function App() {
+  let heroData=[
+    {text1:"Dive Into", text2:"What you Love"},
+    {text1:"Indulge", text2:"Your passions"},
+    {text1:"Give in to", text2:"Your passions"},
+  ]
+  const [heroCount, setHeroCount]=useState(2);
+  const [playStatus, setPlayStatus]=useState(false);
+
+  useEffect(()=>{
+    setInterval(() => {
+      setHeroCount((count)=>{return count===2 ? 0: count+1})
+    }, 3000);
+  },[])
+
+
   return (
     <div>
-      <Header />
-      <p>HomePage Content</p>
-      <Footer/>
+      <Header playStatus={playStatus} heroCount={heroCount}/>
+      <Footer />
+      <Content 
+      heroCount={heroCount}
+      setHeroCount={setHeroCount}
+      playStatus={playStatus}
+      setPlayStatus={setPlayStatus}
+      heroData={heroData[heroCount]}
+      />
     </div>
   );
 }
---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="nav">
+      <div className="nav-logo">EV-Tradition</div>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About</li>
+        <li className='nav-contact'>Contact</li>
+      </ul>
+    </div>
+  );
+}
